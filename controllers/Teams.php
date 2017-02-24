9775ad3f42eeda841537b4797dc3dff7647020fa
Lincoln Brito <lincoln.sbrito@gmail.com>
2017-02-24 10:51:54 -0300

    update form

--- controllers/Teams.php
+++ controllers/Teams.php
@@ -1,7 +1,10 @@
 <?php namespace LincolnBrito\Projects\Controllers;
 
+use Backend\Models\User;
 use BackendMenu;
 use Backend\Classes\Controller;
+use LincolnBrito\Projects\Models\Team;
+use October\Rain\Support\Facades\Flash;
 
 /**
  * Teams Back-end Controller
@@ -21,5 +24,25 @@ class Teams extends Controller
         parent::__construct();
 
         BackendMenu::setContext('LincolnBrito.Projects', 'projects', 'teams');
+    }
+
+    public function create_onSave()
+    {
+        $inputs = post('Team');
+
+        //save the team
+        $teamModel = new Team;
+        $teamModel->name = $inputs['name'];
+        $teamModel->save();
+
+        if(isset($inputs['users']) && count($inputs['users'])>0){
+            // update users team_id
+            User::whereIn('id', $inputs['users'])
+                ->update(['team_id' => $teamModel->id]);
+        }
+
+        Flash::success("Team saved sucessfully");
+
+        return $this->makeRedirect('update',$teamModel);
     }
 }
--- models/Team.php
+++ models/Team.php
@@ -1,12 +1,20 @@
 <?php namespace LincolnBrito\Projects\Models;
 
+use Backend\Models\User;
 use Model;
+use October\Rain\Database\Traits\Validation;
 
 /**
  * Team Model
  */
 class Team extends Model
 {
+    use Validation;
+
+    public $rules = [
+        'name' => 'required'
+    ];
+
     /**
      * @var string The database table used by the model.
      */
@@ -37,4 +45,9 @@ class Team extends Model
     public $morphMany = [];
     public $attachOne = [];
     public $attachMany = [];
+
+    public function getUsersOptions()
+    {
+        return User::lists('login','id');
+    }
 }
